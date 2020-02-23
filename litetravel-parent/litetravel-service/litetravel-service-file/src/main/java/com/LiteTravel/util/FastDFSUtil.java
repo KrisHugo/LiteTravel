dc71d4c2b805da41723b257b625a194b1c063d1f
KrisHugo <KrisHugo@users.noreply.github.com>
2020-02-23 15:41:53 +0800

    更新部分基础内容

--- litetravel-parent/litetravel-service/litetravel-service-file/src/main/java/com/LiteTravel/util/FastDFSUtil.java
+++ litetravel-parent/litetravel-service/litetravel-service-file/src/main/java/com/LiteTravel/util/FastDFSUtil.java
@@ -12,11 +12,11 @@ package com.LiteTravel.util;
 
 import com.LiteTravel.file.FastDFSFile;
 import org.csource.common.NameValuePair;
-import org.csource.fastdfs.ClientGlobal;
-import org.csource.fastdfs.StorageClient;
-import org.csource.fastdfs.TrackerClient;
-import org.csource.fastdfs.TrackerServer;
+import org.csource.fastdfs.*;
 import org.springframework.core.io.ClassPathResource;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
 
 public class FastDFSUtil {
 
@@ -58,8 +58,47 @@ public class FastDFSUtil {
         * uploads[0] 文件上传所储存的Storage的组的名字 -> group1
         * uploads[1] 文件储存到Storage上的文件名字 -> M00/02/44/test.jpg
         * */
-        String[] uploads = storageClient.upload_file(fastDFSFile.getContent(), fastDFSFile.getExt(), meta_list);
-        return uploads;
+        return storageClient.upload_file(fastDFSFile.getContent(), fastDFSFile.getExt(), meta_list);
+    }
+
+    /*
+    * 获取文件信息
+    * */
+    public static FileInfo getFile(String groupName, String remoteFileName) throws Exception{
+        //创建一个Tracker访问的客户端对象TrackerClient
+        TrackerClient trackerClient = new TrackerClient();
+        //通过TrackerClient访问TrackerServer服务, 获取连接信息
+        TrackerServer trackerServer = trackerClient.getConnection();
+        //通过TrackerServer的链接信息可以获取Storage的链接信息, 创建StorageClient对象存储Storage的存储信息
+        StorageClient storageClient = new StorageClient(trackerServer, null);
+        return storageClient.get_file_info(groupName, remoteFileName);
+    }
+
+    /*
+    * 下载文件
+    * */
+    public static InputStream downloadFile(String groupName, String remoteFileName) throws Exception{
+        //创建一个Tracker访问的客户端对象TrackerClient
+        TrackerClient trackerClient = new TrackerClient();
+        //通过TrackerClient访问TrackerServer服务, 获取连接信息
+        TrackerServer trackerServer = trackerClient.getConnection();
+        //通过TrackerServer的链接信息可以获取Storage的链接信息, 创建StorageClient对象存储Storage的存储信息
+        StorageClient storageClient = new StorageClient(trackerServer, null);
+        byte[] buffer = storageClient.download_file(groupName, remoteFileName);
+        return new ByteArrayInputStream(buffer);
+    }
+
+    /*
+    * 删除文件
+    * */
+    public static void deleteFile(String groupName, String remoteFileName) throws Exception{
+        //创建一个Tracker访问的客户端对象TrackerClient
+        TrackerClient trackerClient = new TrackerClient();
+        //通过TrackerClient访问TrackerServer服务, 获取连接信息
+        TrackerServer trackerServer = trackerClient.getConnection();
+        //通过TrackerServer的链接信息可以获取Storage的链接信息, 创建StorageClient对象存储Storage的存储信息
+        StorageClient storageClient = new StorageClient(trackerServer, null);
+        storageClient.delete_file(groupName, remoteFileName);
     }
 
 }
